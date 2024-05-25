fa7f7e6d4042541b5c623c9a3215a77a04e5c749
paruz <p.p.ruzanov@gmail.com>
2024-05-25 12:34:43 +0300

    Done 2

--- Homework1-1.py
+++ Homework1-1.py
@@ -3,4 +3,4 @@ while not n_str.isdigit():
     n_str = input("Введите целое число: ")
 n = int(n_str)
 for i in range(n):
-    print(" " * (n-i-1) + "*" * (2*i-1))
+    print(" " * (n - i - 1) + "*" * (2 * i + 1))
