6b7d1e96312ba5d81c45d6c2d0192b939578fec7
paruz <p.p.ruzanov@gmail.com>
2024-06-20 13:54:45 +0300

    Done 2

--- Homework2-2.py
+++ Homework2-2.py
@@ -3,7 +3,7 @@ try:
         with open('output.txt', 'w') as f2:
             symbols = input('Введите символы для удаления с правого края строки:')
             for line in f1.readlines():
-                line = line.rstrip(symbols + '\n') + ';'
+                line = line.rstrip(symbols + '\n' + ';')
                 f2.write(line[::-1] + '\n')
 except FileNotFoundError:
     print('Файл input.txt не найден')
